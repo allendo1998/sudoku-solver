2547dd2d76ab54c4706a9127f11b5e0ea1b00982
Allen Do <allendo@192-168-1-106.tpgi.com.au>
2022-01-18 21:13:54 +1100

    added clear board function

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,7 @@
 from multiprocessing.sharedctypes import Value
 import tkinter as tk
 
+entries = []
 
 board = [
     [0, 0, 0, 0, 0, 0, 4, 7, 3],
@@ -14,17 +15,29 @@ board = [
     [0, 3, 0, 0, 0, 0, 2, 0, 0],
 ]
 
-def grid_layout(root, grid_dim):
-    entries = []
+def print_sudoku(board):
+    print("-"*37)
+    for i, row in enumerate(board):
+        print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
+        if i == 8:
+            print("-"*37)
+        elif i % 3 == 2:
+            print("|" + "---+"*8 + "---|")
+        else:
+            print("|" + "   +"*8 + "   |")
+
+
+def grid_layout(root, grid_dim, func):
     
     for row in range(grid_dim):
         for col in range(grid_dim):
         
             entry = tk.Entry(root, width=2, highlightthickness=5, highlightbackground='#000000', justify="center")
-            if(board[row][col] == 0):
+            if(func == "clear"):
                 entry.insert(-1," ")
             else:
-                entry.insert(-1,board[row][col])
+                if(board[row][col] != 0):
+                    entry.insert(-1,board[row][col])
             pad_y = (0, 0)
             pad_x = (0, 0)
             
@@ -37,11 +50,24 @@ def grid_layout(root, grid_dim):
             entry.grid(row=row, column=col, ipadx=10, ipady=10, padx=pad_x, pady=pad_y)
             entries.append(entry)
 
+def get_entries():
     return entries
-    
+
+def clear_board():
+    grid_layout(root,9,"clear")
 
 
 root = tk.Tk()
 root.title('Sudoku Solver')
-entries = grid_layout(root, 9)
+grid_layout(root, 9, "None")
+
+# Button To Solve
+# B = tk.Button(root, text ="Solve", command = clear_board)
+# B.grid(row = 13, column = 4)
+
+# Clear Board
+C = tk.Button(root, text = "Clear", comman = clear_board)
+C.grid(row = 0, column = 16)
+
+
 root.mainloop()
