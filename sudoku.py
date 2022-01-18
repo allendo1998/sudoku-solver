d83baa71ba172db4474629c353b0ffc4379d0ea1
Allen Do <allendo@192-168-1-106.tpgi.com.au>
2022-01-18 20:26:31 +1100

    read from board and display on ui

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,47 @@
+from multiprocessing.sharedctypes import Value
 import tkinter as tk
 
 
-def drawGrid(width, height):
-    box_size = width * height
-    print("Drawing Grid" + box_size)
+board = [
+    [0, 0, 0, 0, 0, 0, 4, 7, 3],
+    [0, 0, 0, 0, 0, 9, 0, 0, 0],
+    [0, 0, 0, 8, 0, 1, 9, 0, 6],
+    [9, 0, 0, 0, 2, 7, 5, 6, 8],
+    [6, 8, 0, 0, 0, 0, 0, 0, 2],
+    [0, 5, 0, 0, 8, 0, 0, 0, 7],
+    [4, 0, 9, 0, 3, 0, 0, 0, 0],
+    [0, 6, 0, 5, 0, 8, 0, 0, 0],
+    [0, 3, 0, 0, 0, 0, 2, 0, 0],
+]
 
+def grid_layout(root, grid_dim):
+    entries = []
+    
+    for row in range(grid_dim):
+        for col in range(grid_dim):
+        
+            entry = tk.Entry(root, width=2, highlightthickness=5, highlightbackground='#000000', justify="center")
+            if(board[row][col] == 0):
+                entry.insert(-1," ")
+            else:
+                entry.insert(-1,board[row][col])
+            pad_y = (0, 0)
+            pad_x = (0, 0)
+            
+            if (row+1) % 3 == 0 and (row+1) < grid_dim: 
+                pad_y = (0, 10)
+                
+            if (col+1) % 3 == 0 and (col+1) < grid_dim: 
+                pad_x = (0, 10)
 
+            entry.grid(row=row, column=col, ipadx=10, ipady=10, padx=pad_x, pady=pad_y)
+            entries.append(entry)
 
+    return entries
+    
 
 
 root = tk.Tk()
-root.title("Sudoku Solver")
-canvas = tk.Canvas(root, width=700, height=700, bg="#263D42")
-canvas.pack()
+root.title('Sudoku Solver')
+entries = grid_layout(root, 9)
 root.mainloop()
